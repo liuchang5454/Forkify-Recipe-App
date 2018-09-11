f508319a3964779f406710b5c14e92db276f07c6
liuchang5454 <liuchang5454@gmail.com>
2018-09-10 22:06:10 -0600

    build the shopping list view. Now able to click button ADD TO SHOPPING LIST, and count unit and ingredients are added on the right side.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import Search from "./models/Search";
 import Recipe from "./models/Recipe";
+import List from "./models/List";
 import * as searchView from "./views/searchView";
 import * as recipeView from "./views/recipeView";
+import * as listView from "./views/listView";
 import {elements, renderLoader, clearloader} from "./views/base";
 
 
@@ -109,6 +111,22 @@ const controlRecipe = async () => {
 
 ["hashchange", "load"].forEach(event => window.addEventListener(event, controlRecipe));
 
+
+/*
+LIST CONTROLLER
+*/
+const controlList = () => {
+    //create a new list if there is none yet
+    if(!state.list) state.list = new List();
+
+    //add each ingredient to the list
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item);
+    });
+}
+
+
 //handling recipe button clicks
 elements.recipe.addEventListener("click", e => {
     if(e.target.matches(".btn-decrease, .btn-decrease *")){
@@ -121,6 +139,12 @@ elements.recipe.addEventListener("click", e => {
         //increase button is clicked
         state.recipe.updateServings("inc");
         recipeView.updateServingsIngredients(state.recipe);
+    }else if(e.target.matches(".recipe__btn--add, .recipe__btn--add *")){
+        controlList();
     }
+
+
     console.log(state.recipe);
 });
+
+window.l = new List(); 
